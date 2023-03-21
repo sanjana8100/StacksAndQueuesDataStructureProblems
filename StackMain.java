d7b495bf936d09925177a85788f266671b50790b
sanjana8100 <sanjanams812@gmail.com>
2023-03-21 18:30:14 +0530

    added: UC2

--- StackMain.java
+++ StackMain.java
@@ -7,6 +7,18 @@ public class StackMain {
         stack.push(70);
         stack.push(30);
         stack.push(56);
+        System.out.println("The Stack:");
         stack.show();
+        System.out.println();
+
+        Integer poppedData;
+        do {
+            poppedData = stack.pop();
+            if (poppedData == null)
+                System.out.println("Stack is Empty");
+            else
+                System.out.println("The element popped is => " + poppedData);
+            stack.show();
+        } while( poppedData != null);
     }
 }
